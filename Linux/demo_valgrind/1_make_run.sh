472f8b4161938e12d7484ccbe2a841b5718b915f
hugochang1 <sand208102@gmail.com>
2019-09-03 13:40:21 +0800

    Update 1_make_run.sh

--- Linux/demo_valgrind/1_make_run.sh
+++ Linux/demo_valgrind/1_make_run.sh
@@ -2,5 +2,5 @@ make clean
 make
 #valgrind --leak-check=yes ./test
 valgrind --leak-check=full --log-file=valgrind.log ./test
-#valgrind --leak-check=full --log-file=valgrind.log ./test -l xxx.so
+#valgrind --leak-check=full --log-file=valgrind.log --show-leak-kinds=all ./test -l xxx.so
 cat valgrind.log
