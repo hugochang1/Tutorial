3b034ae8e5d6675ed7f958b6c5edabde77fed8a2
hugochang1 <sand208102@gmail.com>
2020-06-29 08:53:37 +0800

    Update 05_io.lua

--- Lua/05_io.lua
+++ Lua/05_io.lua
@@ -30,16 +30,4 @@ etc
 ...
 --]]
 
------------------ use pcall() to check whether specific module exsits -----------------
-function require_module_if_exists(module_name)
-    if pcall(require, module_name) then
-        return require(module_name)
-    else
-        return nil
-    end
-end
-
-print(require_module_if_exists("io"))  -- table: 0x1242020
-print(require_module_if_exists("io2"))  -- nil
-
 ----------------- other -----------------
