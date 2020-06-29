d6d33109b1c3e706a337b6fa45baf0b9392a4d59
hugochang1 <sand208102@gmail.com>
2020-06-29 08:54:06 +0800

    Update 06_module.lua

--- Lua/06_module.lua
+++ Lua/06_module.lua
@@ -10,4 +10,16 @@ tabledump.dump({aaa="123", bbb="456", ccc="789"})
 }
 --]]
 
+----------------- use pcall() to check whether specific module exsits -----------------
+function require_module_if_exists(module_name)
+    if pcall(require, module_name) then
+        return require(module_name)
+    else
+        return nil
+    end
+end
+
+print(require_module_if_exists("io"))  -- table: 0x1242020
+print(require_module_if_exists("io2"))  -- nil
+
 ----------------- other -----------------
