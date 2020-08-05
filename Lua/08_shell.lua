ee4b74ff19cc57fe5bc09bb4675f7a4673fd0644
hugochang1 <sand208102@gmail.com>
2020-08-05 14:55:52 +0800

    Update 08_shell.lua

--- Lua/08_shell.lua
+++ Lua/08_shell.lua
@@ -3,9 +3,14 @@ os.execute("mkdir myfolder")
 
 ----------------- send command and get result -----------------
 function shell_command(command)
-    local fp = io.popen(command)
-    local txt = fp:read("*a")
-    fp:close()
+	local retry_count = 10
+	local fp, txt, err
+	repeat
+		fp = io.popen(command)
+		txt, err = fp:read("*a")
+		fp:close()
+		retry_count = retry_count - 1
+	until err == nil or retry_count == 0
     return txt
 end
 
