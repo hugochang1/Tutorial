20164789ebe4022a1725e18cc2e7ce9fbc90c9ba
hugochang1 <sand208102@gmail.com>
2021-07-04 18:00:41 +0800

    Update client.py

--- Python/socket/test/client.py
+++ Python/socket/test/client.py
@@ -1,35 +1,77 @@
 import socket
 import time
+import traceback
 
 HOST = '127.0.0.1'
 PORT = 9999
 RECV_DATA_SIZE = 1024 * 16
 
+def show_traceback():
+    for line in traceback.format_stack():
+        print(line.strip())
+
+def read_and_expect_value(s, expect_value):
+	data = s.recv(RECV_DATA_SIZE).decode()
+	if len(data) == 0:
+		print("[Client] ERR: server close the connection")
+		exit(1)
+	if data[0] != expect_value:
+		print("[Client] ERR: test failed")
+		show_traceback()
+		exit(1)
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+# ------------ test 1 ------------
 cmd = "1"
 s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
+read_and_expect_value(s, "0")
 
-cmd = "1"
+cmd = "11"
 s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
+read_and_expect_value(s, "1")
+
+# ------------ test 2 ------------
+cmd = "22"
+s.sendall(cmd.encode())
+read_and_expect_value(s, "0")
 
 cmd = "2"
 s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
-
-cmd = "22"
-s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
+read_and_expect_value(s, "1")
 
 cmd = "20"
 s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
+read_and_expect_value(s, "1")
 
 cmd = "222"
 s.sendall(cmd.encode())
-s.recv(RECV_DATA_SIZE)
+read_and_expect_value(s, "1")
+
+# ------------ test 3 ------------
+cmd = "3"
+for i in range(1000):
+	cmd += "a"
+cmd += "!"
+s.sendall(cmd.encode())
+read_and_expect_value(s, "0")
+
+# ------------ test 4 ------------
+cmd = "4"
+for i in range(2000):
+	cmd += "a"
+cmd += "!"
+s.sendall(cmd.encode())
+read_and_expect_value(s, "0")
+
+# ------------ test 5 ------------
+cmd = "5"
+for i in range(16000):
+	cmd += "a"
+cmd += "!"
+s.sendall(cmd.encode())
+read_and_expect_value(s, "0")
 
 s.close()
