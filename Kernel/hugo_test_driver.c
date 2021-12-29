010dcb4085e952d632d89e88edface7b73adfd31
hugochang1 <sand208102@gmail.com>
2021-12-29 23:06:03 +0800

    Update hugo_test_driver.c

--- Kernel/hugo_test_driver.c
+++ Kernel/hugo_test_driver.c
@@ -1,2 +1,21 @@
+//--------------------- kmalloc ---------------------
+#include <linux/slab.h>
 
-aaaa
+static void kmalloc_example(void)
+{
+	void *data1;
+
+	data1 = kmalloc(1024, GFP_KERNEL);
+	if (data1 == NULL) {
+		pr_err("[hugo] kmalloc() failed 1\n");
+		return;
+	}
+	kfree(data1);
+
+	data1 = kzalloc(1024, GFP_ATOMIC);
+	if (data1 == NULL) {
+		pr_err("[hugo] kmalloc() failed 2\n");
+		return;
+	}
+	kfree(data1);
+}
