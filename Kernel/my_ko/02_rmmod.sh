060e5bb7671ff4cbe85e7a992f3b13b56d282ff2
Hugo Chang <sand208102@gmail.com>
2022-02-16 00:05:34 +0800

    remove unused files

--- Kernel/my_ko/02_rmmod.sh
+++
@@ -1,8 +0,0 @@
-sudo rmmod hello.ko
-case $? in
-	0) dmesg;
-		echo -e "\e[1;32m---------- rmmod hello.ko success -----------\e[m";
-		;;
-	*) echo -e "\e[1;31m---------- rmmod hello.ko failed-----------\e[m";
-		;;
-esac
