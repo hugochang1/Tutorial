488e2e079c605277399bdcf00d15dcc452c1a4eb
Hugo Chang <sand208102@gmail.com>
2022-02-22 22:41:14 +0800

    update example

--- Kernel/tutorial/12_sync/hugo1.c
+++ Kernel/tutorial/12_sync/hugo1.c
@@ -53,7 +53,7 @@ static struct task_struct *task;
 
 static int hugo_thread(void *arg)
 {
-	pr_info("hugo_thread() arg=%px\n", arg); //0x1234
+	pr_info("hugo_thread() arg=%px\n", arg); //NULL
 	mdelay(500);
 	pr_info("complete_and_exit()\n");
 	complete_and_exit(&comp, 0);
@@ -65,30 +65,103 @@ static void demo_completion(void)
 {
 	init_completion(&comp);
 	init_waitqueue_head(&wait);
-	task = kthread_run(hugo_thread, 0x1234, "hugo_thread");
+	task = kthread_run(hugo_thread, NULL, "hugo_thread");
 
 	pr_info("wait_for_completion() before\n");
 	wait_for_completion(&comp);
 	pr_info("wait_for_completion() after\n");
 	/*
-	[ 1488.477842] wait_for_completion() before
-	[ 1488.477852] hugo_thread() num=0000000000000000
-	[ 1488.974089] complete_and_exit()
-	[ 1488.974114] wait_for_completion() after
+	[   74.618656] wait_for_completion() before
+	[   74.618666] hugo_thread() arg=0000000000001234
+	[   75.114893] complete_and_exit()
+	[   75.114938] wait_for_completion() after
 	*/
 	//wait_for_completion() must be called before complete_and_exit(), or wait_for_completion() will be blocked forever
-	//for timeout case, we can use below API instead to avoid blocking forever
+	//we can use  wait_for_completion_timeout to avoid blocking forever
 	//unsigned long wait_for_completion_timeout(struct completion *x, unsigned long timeout);
 }
 
 //-------------------------------------------------------------------------------------------
+static void demo_mutex(void)
+{
+	struct mutex mutex;
+	
+	mutex_init(&mutex);
+	
+	mutex_lock(&mutex);
+	mutex_unlock(&mutex);
+}
+
+//-------------------------------------------------------------------------------------------
+static int hugo_kthread(void *arg)
+{
+	pr_info("hugo_kthread() arg=%px\n", arg); // NULL
+	
+	while (!kthread_should_stop()) {
+		pr_info("hugo_kthread()\n");
+		mdelay(500);
+	}
+	pr_info("hugo_kthread() exit\n");
+	return 0;
+}
+
+static struct task_struct *ktask = NULL;
+
+static void demo_kthread(void)
+{
+# if 0
+	ktask = kthread_create(hugo_kthread, NULL, "hugo_kthread");
+	if (IS_ERR(ktask)) {
+		pr_err("kthread_create() failed\n");
+		//return PTR_ERR(ktask);
+		return;
+	}
+	wake_up_process(ktask); //to start the thread
+#else
+	ktask = kthread_run(hugo_kthread, NULL, "hugo_kthread");
+	if (IS_ERR(ktask)) {
+		pr_err("kthread_run() failed\n");
+		//return PTR_ERR(ktask);
+		return;
+	}
+#endif
+	mdelay(2000);
+	kthread_stop(ktask);
+}
+
+//-------------------------------------------------------------------------------------------
+
+static void demo_spinlock(void)
+{
+	unsigned long flags;
+	spinlock_t lock;
+	
+	spin_lock_init(&lock);
+	
+	spin_lock(&lock); //get lock
+	spin_unlock(&lock);
+	
+	pr_info("spin_trylock()=%d\n", spin_trylock(&lock)); //1
+	pr_info("spin_trylock()=%d\n", spin_trylock(&lock)); //0
+	spin_unlock(&lock);
+	
+	spin_lock_irqsave(&lock, flags); //get lock + disable local processor interrupt 
+	spin_unlock_irqrestore(&lock, flags);
+	
+	//do not suggest to use this, because we didn't know whether interrupt is disabled or not
+	spin_lock_irq(&lock); //get lock + disable local processor interrupt
+	spin_unlock_irq(&lock);
+}
+//-------------------------------------------------------------------------------------------
 static int hugo_init(void)
 {
-	pr_info("hugo_init ver=1.00\n");
-	//demo_atomic();
-	//demo_atomic_bitops();
+	pr_info("hugo_init ver=1.01\n");
+	demo_atomic();
+	demo_atomic_bitops();
 	demo_completion();
-	//TBC
+	demo_mutex();
+	demo_kthread();
+	demo_spinlock();
 	
 	return 0;
 }
@@ -96,7 +169,6 @@ static int hugo_init(void)
 static void hugo_exit(void)
 {
 	pr_info("hugo_exit\n");
-
 }
 
 module_init(hugo_init);
