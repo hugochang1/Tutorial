f77313401056d16512a06b617946801c38d9181b
hugochang1 <sand208102@gmail.com>
2026-02-11 13:27:49 +0800

    Update main.c

--- Programming/C/22_queue/main.c
+++ Programming/C/22_queue/main.c
@@ -13,17 +13,15 @@ typedef struct queue {
 } queue;
 
 void queue_init(queue *q) {
-    q->front = NULL;
-    q->rear = NULL;
+    q->front = q->rear = NULL;
 }
 
 bool queue_empty(queue *q) {
     return q->front == NULL;
 }
 
-bool queue_enqueue(queue *q, int data) {
+bool queue_enqueue(queue* q, int data) {
     list *l = (list*)malloc(sizeof(list));
-    if (l == NULL) return false;
     l->data = data;
     l->next = NULL;
     if (q->front == NULL) {
@@ -35,19 +33,19 @@ bool queue_enqueue(queue *q, int data) {
     return true;
 }
 
-bool queue_dequeue(queue *q, int *data) {
-    if (q->front == NULL) return false;
-    list *tmp = q->front;
-    *data = tmp->data;
-    q->front = tmp->next;
-    free(tmp);
+bool queue_dequeue(queue* q, int *data) {
+    if (queue_empty(q)) return false;
+    list *l = q->front;
+    *data = l->data;
+    q->front = l->next;
+    free(l);
     if (q->front == NULL) q->rear = NULL;
     return true;
 }
 
-void queue_dump(queue *q) {
+void queue_print(queue *q) {
     list *l = q->front;
-    printf("queue_dump() front=%p rear=%p ", q->front, q->rear);
+    printf("queue_print() ");
     while (l != NULL) {
         printf("%d ", l->data);
         l = l->next;
@@ -56,16 +54,16 @@ void queue_dump(queue *q) {
 }
 
 int main() {
-    int i;
     queue q;
+    int i;
+    
     queue_init(&q);
+    queue_print(&q); // queue_print() 
     
-    queue_dump(&q); // queue_dump() front=(nil) rear=(nil) 
-    
-    for (i = 0; i < 8; i++) {
+    for(i = 0; i < 4; i++) {
         queue_enqueue(&q, i);
     }
-    queue_dump(&q); // queue_dump() front=0x92d2c0 rear=0x92d3a0 0 1 2 3 4 5 6 7 
+    queue_print(&q); // queue_print() 0 1 2 3 
     
     while(!queue_empty(&q)) {
         int data = -1;
@@ -76,18 +74,9 @@ int main() {
         ret=1 data=1
         ret=1 data=2
         ret=1 data=3
-        ret=1 data=4
-        ret=1 data=5
-        ret=1 data=6
-        ret=1 data=7
         */
     }
-    queue_dump(&q); // queue_dump() front=(nil) rear=(nil) 
-    
-    for (i = 0; i < 16; i++) {
-        queue_enqueue(&q, i);
-    }
-    queue_dump(&q); // queue_dump() front=0x92d380 rear=0x92d4a0 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
-    
+    queue_print(&q); // queue_print() 
+
     return 0;
 }
