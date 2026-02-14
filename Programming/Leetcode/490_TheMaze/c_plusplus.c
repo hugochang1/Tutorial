438ce2e9766c1399517e2e4a3ff4d4986cc89682
hugochang1 <sand208102@gmail.com>
2026-02-14 16:06:36 +0800

    Update c_plusplus.c

--- Programming/Leetcode/490_TheMaze/c_plusplus.c
+++ Programming/Leetcode/490_TheMaze/c_plusplus.c
@@ -3,21 +3,31 @@
 #include <memory.h>
 
 #define X_NUM 5
-#define Y_NUM 6
+#define Y_NUM 5
 
-int visited[Y_NUM][X_NUM];
-int dirs[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+int visisted[Y_NUM][X_NUM];
+int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+// Time Complexity: O(N x M x max(N, M)), N is the number of row, M is the number of column
 bool dfs(int maze[][X_NUM], int startx, int starty, int endx, int endy) {
     if (startx == endx && starty == endy) return true;
-    if (visited[starty][startx]) return false;
     if (maze[starty][startx]) return false;
+    if (visisted[starty][startx]) return false;
     
-    visited[starty][startx] = 1;
+    visisted[starty][startx] = 1;
     int i;
     for(i = 0; i < 4; i++) {
-        int nx = startx + dirs[i][1];
-        int ny = starty + dirs[i][0];
+        int nx = startx;
+        int ny = starty;
+        
+        while (nx >= 0 && nx < X_NUM && ny >= 0 && ny < Y_NUM && maze[nx][ny] == 0) {
+            nx += dirs[i][1];
+            ny += dirs[i][0];
+        }
+        
+        // step back
+        nx -= dirs[i][1];
+        ny -= dirs[i][0];
         
         if (nx >= 0 && nx < X_NUM && ny >= 0 && ny < Y_NUM) {
             bool ret = dfs(maze, nx, ny, endx, endy);
@@ -28,22 +38,23 @@ bool dfs(int maze[][X_NUM], int startx, int starty, int endx, int endy) {
 }
 
 bool hasPath(int maze[][X_NUM], int startx, int starty, int endx, int endy) {
-    memset(visited, 0, sizeof(visited));
+    memset(visisted, 0, sizeof(visisted));
     return dfs(maze, startx, starty, endx, endy);
 }
 
+//3:53 -> 3:58
+
 int main() {
     int maze[][X_NUM] = {
-        {0, 0, 0, 0, 0},
-        {1, 0, 1, 1, 1},
         {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0},
         {0, 1, 1, 1, 0},
         {0, 1, 0, 0, 0},
         {0, 0, 0, 1, 0},
     };
     
-    bool ret = hasPath(maze, 0, 0, 5, 5);
-    printf("ret=%d\n", ret);
+    bool ret = hasPath(maze, 0, 0, 4, 4);
+    printf("ret=%d\n", ret); // ret=1
     
     return 0;
 }
