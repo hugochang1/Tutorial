f69d9a3379ab9e54949c3011df038a9e066f1655
hugochang1 <sand208102@gmail.com>
2026-02-14 23:22:38 +0800

    Update c_plusplus.c

--- Programming/Leetcode/005_LongestPalindromicSubstr/c_plusplus.c
+++ Programming/Leetcode/005_LongestPalindromicSubstr/c_plusplus.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void find(const char* str, char* out, int size) {
+// Time Complexity: O(n^2)
+void find(const char* str, char* out) {
     int len = strlen(str);
     if (len == 0) return;
     int i, j;
-    int max_count = 1;
-    int max_begin = 0, max_end = 0;
-    for(i = 0; i < len; i++) {
-        for (j = 1; (i - j) >= 0 && (i + j) < size; j++) {
-            int start = i - j;
+    int max_len = 1, max_begin = 0, max_end = 0;
+    for (i = 0; i < len; i++) {
+        for (j = 0; (i - j) >= 0 && (i + j) < len; j++) {
+            int begin = i - j;
             int end = i + j;
-            if (str[start] == str[end]) {
-                int diff = (end - start) + 1;
-                if (diff > max_count) {
-                    max_count = diff;
-                    max_begin = start;
+            if (str[begin] == str[end]) {
+                int curr_len = (end - begin) + 1;
+                if (curr_len > max_len) {
+                    max_len = curr_len;
+                    max_begin = begin;
                     max_end = end;
                 }
             } else {
@@ -24,15 +24,15 @@ void find(const char* str, char* out, int size) {
             }
         }
     }
-    memcpy(out, str + max_begin, max_count);
+    memcpy(out, str + max_begin, max_len);
 }
 
 int main() {
-    const char* str = "abab12567";
+    const char* str = "ababd";
     char out[1024] = {0};
     //expect output: bab or aba
     
-    find(str, out, sizeof(out));
+    find(str, out);
     printf("out=%s\n", out);
     return 0;
 }
