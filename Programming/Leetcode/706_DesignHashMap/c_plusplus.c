4c23b50b21fd0cebb31c16055a42f25de9cddbaf
hugochang1 <sand208102@gmail.com>
2026-02-20 00:09:48 +0800

    Update c_plusplus.c

--- Programming/Leetcode/706_DesignHashMap/c_plusplus.c
+++ Programming/Leetcode/706_DesignHashMap/c_plusplus.c
@@ -4,58 +4,66 @@
 #include <deque>
 #include <utility>
 
-//706 design a HashMap, DesignHashMap
-
 using namespace std;
+//706 design a HashMap, DesignHashMap
 
 class HashMap {
     vector<deque<pair<int, int>>> v;
-    int buck_num;
+    int num_buck;
+    
 public:
     HashMap() {
-        buck_num = 4;
-        for(int i = 0; i < buck_num; i++) {
+        num_buck = 4;
+        for (int i = 0; i < num_buck; i++) {
             deque<pair<int, int>> d;
             v.push_back(d);
         }
     }
-
+    
     void put(int key, int value) {
-        int index = key % buck_num;
-        v[index].push_back({key, value});
+        int hash_value = key % num_buck;
+        deque<pair<int, int>>& d = v[hash_value];
+        bool contain = false;
+        for(auto& [k, v]:d) {
+            if (k == key) {
+                v = value;
+                contain = true;
+            }
+        }
+        if (!contain) {
+            d.push_back({key, value});
+        }
     }
     
     int get(int key) {
-        int index = key % buck_num;
-        deque<pair<int, int>> d = v[index];
+        int hash_value = key % num_buck;
+        deque<pair<int, int>> d = v[hash_value];
         for (auto [k, v]:d) {
-            if (k == key) {
-                return v;
-            }
+            if (k == key) return v;
         }
         return -1;
     }
     
     void remove(int key) {
-        int index = key % buck_num;
-        int index_for_remove = -1;
-        deque<pair<int, int>> &d = v[index];
-        for (int i = 0; i < d.size(); i++) {
+        int hash_value = key % num_buck;
+        deque<pair<int, int>>& d = v[hash_value];
+        int remove_index = -1;
+        for(int i = 0; i < d.size(); i++) {
             if (d[i].first == key) {
-                index_for_remove = i;
+                remove_index = i;
                 break;
             }
         }
-        if (index_for_remove >= 0) {
-            d.erase(d.begin() + index_for_remove);
+        if(remove_index >= 0) {
+            d.erase(d.begin() + remove_index);
         }
     }
     
-    void print() {
-        printf("size=%zu\n", v.size());
-        for(int i = 0; i < v.size(); i++) {
+    void dump() {
+        printf("dump HashMap\n");
+        for (int i = 0; i < v.size(); i++) {
             printf("i=%d size=%zu ", i, v[i].size());
-            for(int j = 0; j < v[i].size(); j++) {
+            for (int j = 0; j < v[i].size(); j++) {
                 printf("%d/%d ", v[i][j].first, v[i][j].second);
             }
             printf("\n");
@@ -66,37 +74,61 @@ public:
 int main() {
     HashMap m;
     
-    m.put(1,11);
-    m.put(17,12);
-    m.put(0, 1);
-    
-    printf("%d\n", m.get(2));   // -1
-    printf("%d\n", m.get(1));   // 11
-    printf("%d\n", m.get(17));  // 12
-    printf("%d\n", m.get(0));   // 1
-    
-    m.print();
+    m.put(1, 11);
+    m.dump();
     /*
-    i=0 size=1 0/1 
-    i=1 size=2 1/11 17/12 
-    i=2 size=0 
-    i=3 size=0 
-    */
-    
-    m.remove(3);
-    m.remove(1);
-    m.remove(17);
-    m.remove(17);
-    m.remove(0);
-    
-    m.print();
-    /*
-    size=4
+    dump HashMap
     i=0 size=0 
-    i=1 size=0 
+    i=1 size=1 1/11 
     i=2 size=0 
     i=3 size=0 
     */
     
+    m.put(1, 12);
+    m.dump();
+    /*
+    dump HashMap
+    i=0 size=0 
+    i=1 size=1 1/12
+    i=2 size=0 
+    i=3 size=0 
+    */
+    
+    m.put(5, 55);
+    m.dump();
+    /*
+    dump HashMap
+    i=0 size=0 
+    i=1 size=2 1/12 5/55 
+    i=2 size=0 
+    i=3 size=0 
+    */
+    
+    m.put(2, 22);
+    m.dump();
+    /*
+    dump HashMap
+    i=0 size=0 
+    i=1 size=2 1/12 5/55 
+    i=2 size=1 2/22 
+    i=3 size=0 
+    */
+    
+    printf("%d\n", m.get(0)); // -1
+    printf("%d\n", m.get(1)); // 12
+    printf("%d\n", m.get(2)); // 22
+    
+    m.remove(0);
+    m.dump(); // no change
+    
+    m.remove(5);
+    m.dump();
+    /*
+    dump HashMap
+    i=0 size=0 
+    i=1 size=1 1/12 
+    i=2 size=1 2/22 
+    i=3 size=0 
+    */
     return 0;
 }
