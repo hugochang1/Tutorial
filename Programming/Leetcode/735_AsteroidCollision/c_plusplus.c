938f7109415cd7dd95299e382c8f24c80172e09d
hugochang1 <sand208102@gmail.com>
2026-02-20 20:39:40 +0800

    Update c_plusplus.c

--- Programming/Leetcode/735_AsteroidCollision/c_plusplus.c
+++ Programming/Leetcode/735_AsteroidCollision/c_plusplus.c
@@ -1,4 +1,3 @@
-
 // Hello world! Cplayground is an online sandbox that makes it easy to try out
 // code.
 
@@ -8,29 +7,22 @@
 #include <stack>
 
 using namespace std;
-// 735_AsteroidCollision
 
-bool getSign(int value) {
-    if (value > 0) return true;
-    return false;
-}
-
-//time complexity: O(n)
-vector<int> find(vector<int>& nums) {
+vector<int> find(vector<int> &input) {
     stack<int> s;
-    
-    for(auto n:nums) {
+    for(auto n:input) {
         if(s.empty()) {
             s.push(n);
         } else {
-            while (!s.empty() && s.top() > 0 && s.top() < -n) {
+            while(!s.empty() && s.top() > 0 && s.top() < -n) {
                 s.pop();
             }
-            if (!s.empty() && s.top() > 0 && s.top() == -n) {
+            if(!s.empty() && s.top() > 0 && s.top() == -n) {
                 s.pop();
-            }
-            if (s.empty() || s.top() < abs(n)) {
-                s.push(n);
+            } else {
+                if(s.empty() || s.top() < abs(n)) {
+                    s.push(n);
+                }
             }
         }
     }
@@ -41,13 +33,12 @@ vector<int> find(vector<int>& nums) {
         ret[--i] = s.top();
         s.pop();
     }
-    
     return ret;
 }
 
-void dump(vector<int> v) {
+void dump(vector<int> &input) {
     printf("dump ");
-    for(auto a:v) {
+    for(auto a:input) {
         printf("%d ", a);
     }
     printf("\n");
@@ -58,9 +49,8 @@ int main() {
     vector<int> v2 = {8, -8};
     vector<int> v3 = {3,5,-6,2,-1,4};
     vector<int> ret;
-    ret = find(v1); dump(ret);
-    ret = find(v2); dump(ret);
-    ret = find(v3); dump(ret);
-    
+    ret = find(v1); dump(ret); // 5 10
+    ret = find(v2); dump(ret); // 
+    ret = find(v3); dump(ret); // -6 2 4
     return 0;
 }
