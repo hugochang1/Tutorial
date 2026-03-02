da178545dc7791689ef09db81ec39f763471b6de
hugochang1 <sand208102@gmail.com>
2026-03-02 15:21:06 +0800

    Update c_plusplus.c

--- Programming/Leetcode/1146_SnapshotArray/c_plusplus.c
+++ Programming/Leetcode/1146_SnapshotArray/c_plusplus.c
@@ -5,50 +5,73 @@
 using namespace std;
 
 //1146_SnapshotArray
-//3:35 -> 3:45
-// TBD: Space Optimization 
+
 class SnapshotArray {
-    vector<int> v;
-    vector<vector<int>> vs;
-    int sanp_count;
+    int snap_count;
+    vector<vector<pair<int, int>>> v;
+    //the first index of v (e.g. v[0]) is stored for current values
+    //the second index of v (e.g. v[i][j]) is stored for history
+    //pair stores snap_id and value
+    
 public:
-    SnapshotArray(int size) {
-        sanp_count = 0;
-        for(int i = 0; i < size; i++) {
-            v.push_back(0);
-        }
+    SnapshotArray(int length) {
+        snap_count = 0;
+        v.resize(length);
     }
     
-    void set(int index, int val) {
-        if(index >= v.size()) {
-            return;
+    void set(int index, int value) {
+        if(v[index].size() == 0) {
+            v[index].push_back({-1, value});
+        } else {
+            v[index][0] = {-1, value};
         }
-        v[index] = val;
     }
     
     int snap() {
-        sanp_count++;
-        vs.push_back(v);
-        return sanp_count - 1;
+        for(int i = 0; i < v.size(); i++) {
+            vector<pair<int,int>>& a = v[i];
+            if(a.size() > 0) {
+                a.push_back({snap_count, a[0].second});
+            }
+        }
+        snap_count++;
+        return snap_count - 1;
     }
     
     int get(int index, int snap_id) {
-        if(index >= v.size()) return -1;
-        if(snap_id >= sanp_count) return -1;
-        return vs[snap_id][index];
+        vector<pair<int,int>> a = v[index];
+        for(auto [record, val]:a) {
+            if(record == snap_id) {
+                return val;
+            }
+        }
+        return 0;
+    }
+    
+    void dump() {
+        printf("len=%zu\n", v.size());
+        for(int i = 0; i < v.size(); i++) {
+            vector<pair<int,int>> a = v[i];
+            printf("  index=%d ", i);
+            for(auto [record, value]:a) {
+                printf("%d/%d ", record, value);
+            }
+            printf("\n");
+        }
     }
 };
 
 int main() {
     SnapshotArray s(3);
-    
-    s.set(0, 3);
-    int id1 = s.snap();
     s.set(0, 5);
-    int id2 = s.snap();
-    printf("id=%d %d\n", id1, id2);
-    printf("%d\n", s.get(0, id1)); // 3
-    printf("%d\n", s.get(0, id2)); // 5
+    printf("id=%d\n", s.snap()); // 0
+    s.set(0, 6);
+    printf("%d\n", s.get(0, 0)); // 5
+    printf("id=%d\n", s.snap()); // 1
+    printf("%d\n", s.get(0, 0)); // 5
+    printf("%d\n", s.get(0, 1)); // 6
+    
+    s.dump();
     
     return 0;
 }
