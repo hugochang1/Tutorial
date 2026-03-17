a693fc41dac6cff5e33bc37685f9268bac0c36e5
hugochang1 <sand208102@gmail.com>
2026-03-17 08:41:48 +0800

    Update c_plusplus.c

--- Programming/Leetcode/2342_MaxSumOfAPairWithEqualSumOfDigits/c_plusplus.c
+++ Programming/Leetcode/2342_MaxSumOfAPairWithEqualSumOfDigits/c_plusplus.c
@@ -9,25 +9,25 @@ using namespace std;
 
 // go through all inputs and save the sum of digits into unordered_map
 // time complexity: O(n^2)
-int find(vector<int> v) {
-    unordered_map<int, vector<int>> m;
+int find(vector<int> inputs) {
     int out = -1;
+    unordered_map<int, vector<int>> m;
     
-    for(auto a:v) {
+    for(auto value:inputs) {
         int sum = 0;
-        int tmp = a;
-        while(tmp != 0) {
-            sum += (tmp % 10);
+        int tmp = value;
+        while(tmp > 0) {
+            sum += tmp % 10;
             tmp = tmp / 10;
         }
         
         if(m.find(sum) != m.end()) {
-            vector<int> v1 = m[sum];
-            for(auto v2:v1) {
-                out = max(out, v2 + a);
+            vector<int> matches = m[sum];
+            for(auto match:matches) {
+                out = max(out, match + value);
             }
         }
-        m[sum].push_back(a);
+        m[sum].push_back(value);
     }
     return out;
 }
