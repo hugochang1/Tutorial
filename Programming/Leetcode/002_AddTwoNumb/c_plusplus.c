346a5f64e0d04714236959daa4dff7f815b8b20e
hugochang1 <sand208102@gmail.com>
2026-03-20 07:02:17 +0800

    Update c_plusplus.c

--- Programming/Leetcode/002_AddTwoNumb/c_plusplus.c
+++ Programming/Leetcode/002_AddTwoNumb/c_plusplus.c
@@ -1,42 +1,40 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
 
 using namespace std;
 
-vector<int> AddTwonNum(const vector<int>& v1, const vector<int>& v2) {
-    vector<int> ret;
-    int m = max(v1.size(), v2.size());
-    int carry = 0;
-    int sum = 0;
-    
-    for(int i = 0; i <= m; i++) {
-        sum = carry;
-        carry = 0;
-        if (i < v1.size()) {
-            sum += v1[i];
-        }
-        if (i < v2.size()) {
-            sum += v2[i];
-        }
-        carry = sum/10;
-        sum = sum % 10;
-        
-        if (i == m && sum == 0 && carry == 0) break;
-        ret.push_back(sum);
+list<int> solution(list<int> l1, list<int> l2) {
+    list<int> ret;
+    int v1 = 0;
+    int v2 = 0;
+    while(!l1.empty()) {
+        v1 *= 10;
+        v1 += l1.back();
+        l1.pop_back();
+    }
+    while(!l2.empty()) {
+        v2 *= 10;
+        v2 += l2.back();
+        l2.pop_back();
+    }
+    int v = v1 + v2;
+    while(v > 0) {
+        int value = v % 10;
+        ret.push_back(value);
+        v /= 10;
     }
     return ret;
 }
 
 int main() {
-    vector<int> v1 = {3,4,3};
-    vector<int> v2 = {2,6,4};
-    
-    vector<int> ret = AddTwonNum(v1, v2);
-    for (auto a : ret) {
-        printf("%d ", a); // 5 0 8 
+    list<int> ret;
+    ret = solution({2,4,3}, {5,6,4});
+    for(auto a:ret) {
+        printf("%d ", a);
     }
     printf("\n");
+    // {7,0,8}
     
     return 0;
 }
