a3125f94191d372ea1ef5503055a9631b3f54766
hugochang1 <sand208102@gmail.com>
2026-03-31 09:09:18 +0800

    Update c_plusplus.c

--- Programming/Leetcode/611_ValidTriangleNumber/c_plusplus.c
+++ Programming/Leetcode/611_ValidTriangleNumber/c_plusplus.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
-bool isValidTriangle(int a, int b, int c) {
-    if ((a+b) > c && (a+c) > b && (b+c) > a) return true;
-    return false;
-}
-
-int cmpfun(const void* a, const void *b) {
-    return *(int*)a - *(int*)b;
-}
-
-// time complexity: O(n^2)
-int solution(int *arr, int size) {
-    int count = 0;
+using namespace std;
+//611_ValidTriangleNumber
+//time complexity: O(n^2)
+int find(vector<int> nums) {
+    int cnt = 0;
+    sort(nums.begin(), nums.end()); //O(nlogn)
     
-    qsort(arr, size, sizeof(int), cmpfun);//O(nlogn)
-
-    for(int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    
-    for(int i = size - 1; i >= 2 ; i--) {
+    for(int i = nums.size() - 1; i >= 2; i--) {
         int left = 0;
         int right = i - 1;
-        
-        while (left < right) {
-            if (arr[left] + arr[right] > arr[i]) {
-                count += (right - left);
+        while(left != right) {
+            if(nums[left] + nums[right] > nums[i]) {
+                cnt += (right - left);
                 right--;
             } else {
                 left++;
@@ -35,15 +22,11 @@ int solution(int *arr, int size) {
         }
     }
     
-    return count;
+    return cnt;
 }
 
-
 int main() {
-    int arr[] = {2,2,3,4}; // ret=3
-    //int arr[] = {4,2,3,4}; // ret=4
-    int ret = solution(arr, sizeof(arr)/sizeof(arr[0]));
-    printf("ret=%d\n", ret);
-    
+    printf("%d\n", find({2,2,3,4}));//3
+    printf("%d\n", find({4,2,3,4}));//4
     return 0;
 }
