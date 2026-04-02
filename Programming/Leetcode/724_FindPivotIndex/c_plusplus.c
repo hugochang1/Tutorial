2eecb58866c07b3b6f506d6f95dbfe76afb2a64d
hugochang1 <sand208102@gmail.com>
2026-04-02 08:51:08 +0800

    Update c_plusplus.c

--- Programming/Leetcode/724_FindPivotIndex/c_plusplus.c
+++ Programming/Leetcode/724_FindPivotIndex/c_plusplus.c
@@ -1,36 +1,26 @@
-// Hello world! Cplayground is an online sandbox that makes it easy to try out
-// code.
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <vector>
 #include <numeric>
 
 using namespace std;
 
-// 724_FindPivotIndex
-
+//724_FindPivotIndex
 //time complexity: O(n)
-int findPivotIndex2(vector<int> &nums) {
-    int right_sum = accumulate(nums.begin(), nums.end(), 0);
-    int left_sum = 0;
-    int i;
-    for(i = 0; i < nums.size(); i++) {
-        right_sum -= nums[i];
-        if(right_sum == left_sum) return i;
-        left_sum += nums[i];
+int find(vector<int> nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    int leftSum = 0;
+    for(int i = 0; i < nums.size(); i++) {
+        sum -= nums[i];
+        if(leftSum == sum) return i;
+        leftSum += nums[i];
     }
     return -1;
 }
 
 int main() {
-    vector<int> v1 = {1,7,3,6,5,6}; // 3
-    vector<int> v2 = {1,2,3}; // -1
-    vector<int> v3 = {2,1,-1}; // 0
-    
-    printf("%d\n", findPivotIndex2(v1)); // 3
-    printf("%d\n", findPivotIndex2(v2)); // -1
-    printf("%d\n", findPivotIndex2(v3)); // 0
-    
+    printf("%d\n", find({1,7,3,6,5,6})); //3
+    printf("%d\n", find({1,2,3})); //-1
+    printf("%d\n", find({2,1,-1})); //0
     return 0;
 }
+
