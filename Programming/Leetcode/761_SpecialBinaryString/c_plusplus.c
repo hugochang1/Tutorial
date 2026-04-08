7431eb21b5b3c09e4b71edf01357fab306ecc5e2
hugochang1 <sand208102@gmail.com>
2026-04-08 08:16:16 +0800

    Update c_plusplus.c

--- Programming/Leetcode/761_SpecialBinaryString/c_plusplus.c
+++ Programming/Leetcode/761_SpecialBinaryString/c_plusplus.c
@@ -1,44 +1,45 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <vector>
+#include <stdlib.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 //761_SpecialBinaryString
 
 string find(string s) {
-    int i = 0;
-    int j = 0;
-    int count = 0;
     vector<string> v;
-    for(i = 0; i < s.size(); i++) {
-        if(s[i] == '0') {
-            count--;
-        } 
-        if(s[i] == '1') {
-            count++;
+    int cnt = 0;
+    int start = 0;
+    
+    for(int i = 0; i < s.size(); i++) {
+        char ch = s[i];
+        if(ch == '0') {
+            cnt--;
+        } else if(ch == '1') {
+            cnt++;
         }
-        if(count == 0) {
-            string inner_s = s.substr(j+1, i-j-1);
-            string max_inner_s = find(inner_s);
-            v.push_back("1" + max_inner_s + "0");
-            j = i + 1;
+        
+        if(cnt == 0) {
+            string sub = find(s.substr(start + 1, i - start - 1));
+            v.push_back('1' + sub + '0');
+            start = i + 1;
         }
     }
     
-    sort(v.rbegin(), v.rend());
-    string out;
+    sort(v.begin(), v.end(), greater<string>());
     
+    string out;
     for(auto a:v) {
         out += a;
     }
+    
     return out;
 }
 
 int main() {
-    string s = "11011000";
-    printf("%s\n", find(s).c_str());
-    //output 11100100
+    printf("%s\n", find("11011000").c_str()); //"11100100"
+    printf("%s\n", find("10").c_str()); //"10"
+    
     return 0;
 }
