838075a9aa0bc2797ca0f5ab64f1955f05a7f4f7
hugochang1 <sand208102@gmail.com>
2026-04-09 08:38:17 +0800

    Update c_plusplus.c

--- Programming/Leetcode/841_KeysAndRooms/c_plusplus.c
+++ Programming/Leetcode/841_KeysAndRooms/c_plusplus.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-#include <queue>
+#include <deque>
 #include <unordered_set>
 
 using namespace std;
 
 //841_KeysAndRooms
-
-// Time complexity: O(MxN), which M is the number of the room and N is the number of key
-// Space complexity: O(max(K, MxN)), which K is the number of bucket
-bool find(vector<vector<int>> r) {
-    // use queue/deque to store the keys while traveling the rooms
-    // to reduce time complexity, use HashMap/unordered_set to the visited rooms
-    deque<int> keys;
+//time complexity: O(m x n), n is the number of rooms, m is the number of keys in rooms
+bool find(vector<vector<int>> rooms) {
+    //use unordered_set for visited rooms
+    //use queue to handle the keys
+    deque<int> q;
+    q.push_back(0);
     unordered_set<int> visited;
     
-    for(auto key:r[0]) {
-        keys.push_back(key);
-        visited.insert(0);
-    }
-    if (visited.size() == r.size()) return true;
-    
-    while(!keys.empty()) {
-        int key = keys.front();
-        keys.pop_front();
-        
-        if(visited.find(key) == visited.end()) {
-            visited.insert(key);
-            for(auto new_key:r[key]) {
-                keys.push_back(new_key);
+    while(!q.empty()) {
+        int k = q.front();
+        q.pop_front();
+        visited.insert(k);
+        if(visited.size() == rooms.size()) return true;
+        for(int key:rooms[k]) {
+            if(visited.count(key) == 0) {
+                q.push_back(key);
             }
-            if (visited.size() == r.size()) return true;
         }
     }
     return false;
 }
 
 int main() {
-    vector<vector<int>> r1 = {{1},{2},{3},{}}; //true
-    vector<vector<int>> r2 = {{1,3},{3,0,1},{2},{0}}; //false
-    
-    printf("%d\n", find(r1));
-    printf("%d\n", find(r2));
+    printf("%d\n", find({{1},{2},{3},{}})); // true
+    printf("%d\n", find({{1,3},{3,0,1},{2},{0}})); // false
     return 0;
 }
