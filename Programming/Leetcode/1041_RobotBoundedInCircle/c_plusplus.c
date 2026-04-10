285fad7216c14cbe80f317cc615a3af0a4b1d4a4
hugochang1 <sand208102@gmail.com>
2026-04-10 09:14:19 +0800

    Update c_plusplus.c

--- Programming/Leetcode/1041_RobotBoundedInCircle/c_plusplus.c
+++ Programming/Leetcode/1041_RobotBoundedInCircle/c_plusplus.c
@@ -6,34 +6,33 @@
 using namespace std;
 
 //1041_RobotBoundedInCircle
-//time complexity: O(n) which n is the length of instruction string
-bool find(string instructions) {
-    vector<vector<int>> moves = {{0,1}, {1,0}, {0,-1}, {-1,0}}; //north, east, south, west
-    vector<int> pos = {0, 0};
-    int dir = 0; // 0=north, 1=east, 2=south, 3=west
+
+bool find(string dirs) {
+    int x = 0, y = 0;
+    int cnt = 0;
     
-    for(char c:instructions) {
-        switch(c) {
-            case 'G':
-                pos[0] += moves[dir][0];
-                pos[1] += moves[dir][1];
-                break;
-            case 'R':
-                dir = (dir + 1) % 4;
-                break;
-            case 'L':
-                dir = (dir + 3) % 4;
-                break;
+    vector<vector<int>> moves = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    for(auto dir:dirs) {
+        if(dir == 'L') {
+            cnt = (cnt + 3) % 4;
+        } else if(dir == 'R') {
+            cnt = (cnt + 1) % 4;
+        } else {
+            x += moves[cnt][0];
+            y += moves[cnt][1];
         }
     }
-    if(pos[0] == 0 && pos[1] == 0) return true;
-    if(dir != 0) return true;
-    return false;
+    
+    if(x==0 && y==0) return true;
+    if(cnt == 0) return false;
+    return true;
 }
 
 int main() {
     printf("%d\n", find("GGLLGG"));  //1
     printf("%d\n", find("GG"));  //0
-    printf("%d\n", find("GL"));  //0
+    printf("%d\n", find("GL"));  //1
+    printf("%d\n", find("LLLL"));  //1
     return 0;
 }
