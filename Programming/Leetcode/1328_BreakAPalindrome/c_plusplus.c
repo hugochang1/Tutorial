16c9eff0f090846171d379d9833adf4fde95d615
hugochang1 <sand208102@gmail.com>
2026-04-13 11:27:54 +0800

    Update c_plusplus.c

--- Programming/Leetcode/1328_BreakAPalindrome/c_plusplus.c
+++ Programming/Leetcode/1328_BreakAPalindrome/c_plusplus.c
@@ -6,23 +6,22 @@ using namespace std;
 
 //1328_BreakAPalindrome
 
-string find(string s) {
-    int size = s.size();
-    if(size <= 1) return "";
-    for(int i = 0; i < size/2; i++) {
-        if(s[i] != 'a') {
-            s[i] = 'a';
-            return s;
+string find(string str) {
+    if(str.size() <= 1) return "";
+    for(int i = 0; i < str.size()/2; i++) {
+        if(str[i] != 'a') {
+            str[i] = 'a';
+            return str;
         }
     }
-    s.back() = 'b';
-    return s;
+    str.back() = 'b';
+    return str;
 }
 
 int main() {
     printf("%s\n", find("a").c_str()); //
     printf("%s\n", find("aa").c_str()); // ab
+    printf("%s\n", find("aaaaaa").c_str()); // aaaaab
     printf("%s\n", find("abccba").c_str()); //aaccba
-    
     return 0;
 }
