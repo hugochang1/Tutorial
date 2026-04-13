f4d792beafcf78efeefb9b0a4f2baec8aba6aa33
hugochang1 <sand208102@gmail.com>
2026-04-13 08:38:48 +0800

    Update c_plusplus.c

--- Programming/Leetcode/1197_MinimumKnightMoves/c_plusplus.c
+++ Programming/Leetcode/1197_MinimumKnightMoves/c_plusplus.c
@@ -3,55 +3,43 @@
 #include <vector>
 #include <deque>
 #include <set>
-#include <unordered_set>
-#include <string>
+
 using namespace std;
 
-//1197_MinimumKnightMoves
-
-vector<vector<int>> moves = {{2,1},{2,-1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2}};
-
-//use BFS (Breadth First Search)
-//Time complexity: O(nlogn) can be improved to O(n) if change set to unordered_set
-int find(int x, int y) {
-    int step = 0;
+//use BFS (Breadth First Seach)
+int find(int target_x, int target_y) {
+    int steps = 0;
     
     deque<pair<int,int>> q;
+    q.push_back({0, 0});
     set<pair<int,int>> visited;
+    visited.insert({0,0});
     
-    unordered_set<pair<int,int>, pair_hash> a;
-    
-    if(x == 0 && y == 0) return 0;
-    
-    for(auto move:moves) {
-        visited.insert({move[0], move[1]});
-        q.push_back({move[0], move[1]});
-    }
+    vector<pair<int,int>> moves = {{2,1},{2,-1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2}};
     
     while(!q.empty()) {
-        step++;
         int size = q.size();
         for(int i = 0; i < size; i++) {
-            pair<int, int> pos = q.front();
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop_front();
-            
-            if(pos.first == x && pos.second == y) return step;
-            
-            for(auto move:moves) {
-                int next_x = pos.first + move[0];
-                int next_y = pos.second + move[1];
-                if(visited.find({next_x, next_y}) == visited.end()) {
-                    visited.insert({next_x, next_y});
+            if(x == target_x && y == target_y) return steps;
+            for(auto [move_x, move_y]:moves) {
+                int next_x = x + move_x;
+                int next_y = y + move_y;
+                if(visited.count({next_x, next_y}) == 0) {
                     q.push_back({next_x, next_y});
+                    visited.insert({next_x, next_y});
                 }
             }
         }
+        steps++;
     }
     return -1;
 }
 
 int main() {
-    printf("%d\n", find(2,1));
-    printf("%d\n", find(5,5));
+    printf("%d\n", find(2,1)); //1
+    printf("%d\n", find(5,5)); //4
     return 0;
 }
