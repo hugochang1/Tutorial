9e94d1201a2c1fa63b23432441b6ccc8ac9d93ea
hugochang1 <sand208102@gmail.com>
2026-04-29 08:16:16 +0800

    Update c_plusplus.c

--- Programming/Leetcode/sorting/c_plusplus.c
+++ Programming/Leetcode/sorting/c_plusplus.c
@@ -38,8 +38,31 @@ vector<int> insertionSort(vector<int> nums) {
     return nums;
 }
 
+
 // time complexity: average case: O(nlogn), worst csae: O(n^2)
+int partition(vector<int>& nums, int low, int high) {
+    int pivot = nums[high];
+    int j = low;
+    for(int i = low; i < high; i++) {
+        if(nums[i] <= pivot) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+    swap(nums[j], nums[high]);
+    return j;
+}
+
+void quickSortRecu(vector<int>& nums, int low, int high) {
+    if(low >= high) return;
+    
+    int pivot = partition(nums, low, high);
+    quickSortRecu(nums, low, pivot - 1);
+    quickSortRecu(nums, pivot + 1, high);
+}
+
 vector<int> quickSort(vector<int> nums) {
+    quickSortRecu(nums, 0, nums.size() - 1);
     return nums;
 }
 
@@ -51,10 +74,52 @@ vector<int> mergeSort(vector<int> nums) {
 int main() {
     vector<int> ret;
     
-    ret = insertionSort({3,2,1,4,5,0,-1});
+    ret = quickSort({3,2,1,4,5,0,-1});
     for(auto a:ret) {
         printf("%d ", a);
     } // -1 0 1 2 3 4 5 
+    printf("\n");
+    
+    ret = quickSort({1});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({1, 2});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({2, 1});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({1,2,3});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({1,3,2});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({2,1,3});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
+    printf("\n");
+    
+    ret = quickSort({2,3,1});
+    for(auto a:ret) {
+        printf("%d ", a);
+    }
     printf("\n");
     return 0;
 }
