da03f5ca50a8651255487bc8aa307be1e8ce8bb8
hugochang1 <sand208102@gmail.com>
2026-05-18 08:12:16 +0800

    Create 0034_TrappingRainWater.cpp

---
+++ Programming/Leetcode/0034_TrappingRainWater.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+//0034_TrappingRainWater
+//Time complexity: O(n)
+int solution(vector<int> height) {
+    int ret = 0;
+    int left = 0;
+    int right = height.size() - 1;
+    int maxLeft = 0;
+    int maxRight = 0;
+    
+    while(left <= right) {
+        if(height[left] < height[right]) {
+            //handle left part
+            if(maxLeft < height[left]) {
+                maxLeft = height[left];
+            } else {
+                ret += maxLeft - height[left];
+            }
+            left++;
+        } else {
+            //hanlde right part
+            if(maxRight < height[right]) {
+                maxRight = height[right];
+            } else {
+                ret += maxRight - height[right];
+            }
+            right--;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    printf("%d\n", solution({0,1,0,2,1,0,1,3,2,1,2,1})); // 6
+    return 0;
+}
