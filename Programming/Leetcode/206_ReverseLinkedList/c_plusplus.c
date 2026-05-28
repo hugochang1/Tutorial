b52f1f2fdbd14d789ff370c5e8443f9839d8476e
hugochang1 <sand208102@gmail.com>
2026-05-28 08:04:14 +0800

    Update c_plusplus.c

--- Programming/Leetcode/206_ReverseLinkedList/c_plusplus.c
+++ Programming/Leetcode/206_ReverseLinkedList/c_plusplus.c
@@ -1,38 +1,39 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <list>
 
 using namespace std;
+//0206_ReverseLinkedList
 
-list<int> ReverseLinkedList(list<int> l) {
+list<int> solution(list<int> l) {
     list<int> ret;
-    for(auto a : l) {
-        ret.push_front(a);
+    for(auto& i:l) {
+        ret.push_front(i);
     }
     return ret;
 }
 
-void ReverseLinkedListRecu(list<int> l, list<int>& ret) {
-    if(l.size() == 0) return;
-    int a = l.front();
-    l.pop_front();
-    ReverseLinkedListRecu(l, ret);
-    ret.push_back(a);
+//---------------------------------------------------------
+void recu(list<int>& l, list<int>::iterator it, list<int>& ret) {
+    if(it == l.end()) return;
+    ret.push_front(*it);
+    it++;
+    recu(l, it, ret);
+}
+
+list<int> solutionRecu(list<int> l) {
+    list<int> ret;
+    recu(l, l.begin(), ret);
+    return ret;
 }
 
 int main() {
-    list<int> l = {1,2,3,4,5};
+    list<int> l;
+    l = solutionRecu({1,2,3,4,5});
+    for(auto& i:l) {
+        printf("%d ", i);
+    }
+    printf("\n");
     
-    auto ret = ReverseLinkedList(l);
-    for(auto a:ret) {
-        printf("%d ", a); // 5 4 3 2 1 
-    }
-    printf("\n");
-
-    list<int> ret2;
-    ReverseLinkedListRecu(l, ret2);
-    for(auto a:ret2) {
-        printf("%d ", a); // 5 4 3 2 1 
-    }
-    printf("\n");
     return 0;
 }
