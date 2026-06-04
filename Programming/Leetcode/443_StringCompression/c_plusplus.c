61e0154849dfa2f49114684a2039db05ba7cc621
hugochang1 <sand208102@gmail.com>
2026-06-04 08:48:13 +0800

    Update c_plusplus.c

--- Programming/Leetcode/443_StringCompression/c_plusplus.c
+++ Programming/Leetcode/443_StringCompression/c_plusplus.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string>
 
 using namespace std;
-
 //443_StringCompression
 
-void solution(string& s) {
-    if(s.size() == 0) return;
-    int read_pos = 0;
-    int write_pos = 0;
-    
-    while(read_pos < s.size()) {
-        int current_char = s[read_pos++];
+void solution(string& str) {
+    int i = 0;
+    int j = 0;
+
+    while(i < str.size()) {
         int cnt = 1;
-        
-        while(read_pos < s.size() && current_char == s[read_pos]) {
-            read_pos++;
+        j++;
+        while(i+1 < str.size() && str[i+1] == str[i]) {
             cnt++;
+            i++;
         }
-        
-        s[write_pos++] = current_char;
         if(cnt > 1) {
             string cnt_str = to_string(cnt);
-            for(auto c:cnt_str) {
-                s[write_pos++] = c;
+            for(auto& s:cnt_str) {
+                str[j] = s;
+                j++;
             }
         }
+        i++;
     }
-    s[write_pos++] = 0;
+    str[j] = 0;
 }
 
 int main() {
@@ -43,5 +39,6 @@ int main() {
     s = "abbbbbbbbbbbb";
     solution(s);
     printf("%s\n", s.c_str()); // ab12
+
     return 0;
 }
