cc0c891c1e0fb529736d029b7256557bfb91ead6
hugochang1 <sand208102@gmail.com>
2026-06-05 08:11:10 +0800

    Update c_plusplus.c

--- Programming/Leetcode/490_TheMaze/c_plusplus.c
+++ Programming/Leetcode/490_TheMaze/c_plusplus.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <vector>
+#include <deque>
+#include <set>
 
 using namespace std;
-
 //490_TheMaze
 
-bool bfs(vector<vector<int>>& maze, vector<vector<bool>>& visited, int x, int y, int target_x, int target_y) {
-    int m = maze.size();
-    int n = maze[0].size();
-    
-    visited[x][y] = true;
-    
-    if(x == target_x && y == target_y) return true;
-    
-    vector<vector<int>> moves = {{0,1},{0,-1},{1,0},{-1,0}};
-    
-    for(auto move:moves) {
-        int next_x = x + move[0];
-        int next_y = y + move[1];
-        
-        while(0 <= next_x && next_x < m && 0 <= next_y && next_y < n && maze[next_x][next_y] == 0) {
-            next_x += move[0];
-            next_y += move[1];
-        }
-        next_x -= move[0];
-        next_y -= move[1];
-        
-        if(!visited[next_x][next_y]) {
-            if(bfs(maze, visited, next_x, next_y, target_x, target_y)) return true;
-        }
-    }
-    return false;
-}
-
+//Time complexity: O(mn), space complexity:O(mn)
 bool find(vector<vector<int>> maze, vector<int> start_pos, vector<int> end_pos) {
     int m = maze.size();
     int n = maze[0].size();
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
-    return bfs(maze, visited, start_pos[0], start_pos[1], end_pos[0], end_pos[1]);
+    deque<pair<int,int>> q;
+    set<pair<int,int>> visited;
+
+    q.push_back({start_pos[0], start_pos[1]});
+    visited.insert({start_pos[0], start_pos[1]});
+
+    vector<pair<int,int>> directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    while(!q.empty()) {
+        int size = q.size();
+        for(int i = 0; i < size; i++) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop_front();
+
+            if(x == end_pos[0] && y == end_pos[1]) return true;
+
+            for(auto& dir:directions) {
+                int move_x = dir.first;
+                int move_y = dir.second;
+                int next_x = x + move_x;
+                int next_y = y + move_y;
+
+                while(0 <= next_x && next_x < m && 0 <= next_y && next_y < n && maze[next_x][next_y] == 0) {
+                    next_x += move_x;
+                    next_y += move_y;
+                }
+                next_x -= move_x;
+                next_y -= move_y;
+                if(visited.count({next_x,next_y}) == 0) {
+                    q.push_back({next_x,next_y});
+                    visited.insert({next_x,next_y});
+                }
+            }
+        }
+    }
+    return false;
 }
 
 int main() {
