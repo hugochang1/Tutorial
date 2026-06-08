043d78172b0d842888168879c4bb7ed17d1a8c02
hugochang1 <sand208102@gmail.com>
2026-06-08 08:24:19 +0800

    Update 0560_subarrySumEqualsK.cpp

--- Programming/Leetcode/NV/0560_subarrySumEqualsK.cpp
+++ Programming/Leetcode/NV/0560_subarrySumEqualsK.cpp
@@ -1,35 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
+//0560_subarrySumEqualsK
 
-// time complexity: O(n), space complexity: O(n)
-int solution(vector<int> input, int target) {
-    int i = 0;
+//time complexity: O(n)
+int solution(vector<int> nums, int k) {
+    int cnt = 0;
     int sum = 0;
-    int count = 0;
-    unordered_map<int, int> m; // sum, count
-    // I record all sum data from iteration i to unordered_map
-    // if I can find the summary from unordered_map, j which means 
-    // index j+1 to index i can be sumed to target value
-    
+    unordered_map<int, int> m; //sum, count
     m[0] = 1;
-    
-    for (i = 0; i < input.size(); i++) {
-        sum += input[i];
-        if (m.count(sum - target)) {
-            count += m.count(sum - target);
+
+    for(auto& num:nums) {
+        sum += num;
+        if(m.count(sum - k)) {
+            cnt += m[sum - k];
         }
         m[sum]++;
     }
-    return count;
+    return cnt;
 }
 
 int main() {
-    vector<int> v = {1,2,3,3,3,2,1};
-    int ret = solution(v, 3);
-    printf("ret=%d\n", ret);
+    printf("%d\n", solution({1,1,1}, 2)); //2
+    printf("%d\n", solution({1,2,3}, 3)); //2
     return 0;
 }
