e34a12be1de718d877b3c0714fc00c54e29e1d5b
hugochang1 <sand208102@gmail.com>
2026-06-09 08:03:39 +0800

    Update 0761_SpecialBinaryString.cpp

--- Programming/Leetcode/NV/0761_SpecialBinaryString.cpp
+++ Programming/Leetcode/NV/0761_SpecialBinaryString.cpp
@@ -1,45 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
 //761_SpecialBinaryString
 
-string find(string s) {
+//time complexity: O(n^2), space complexity: O(n^2)
+string find(string str) {
+    string ret;
     vector<string> v;
     int cnt = 0;
-    int start = 0;
-    
-    for(int i = 0; i < s.size(); i++) {
-        char ch = s[i];
-        if(ch == '0') {
-            cnt--;
-        } else if(ch == '1') {
-            cnt++;
-        }
-        
+    int begin = 0;
+
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == '0') cnt--;
+        if(str[i] == '1') cnt++;
         if(cnt == 0) {
-            string sub = find(s.substr(start + 1, i - start - 1));
-            v.push_back('1' + sub + '0');
-            start = i + 1;
+            string substr = find(str.substr(begin + 1, i - begin - 1));
+            v.push_back("1" + substr + "0");
+            begin = i+1;
         }
     }
-    
-    sort(v.begin(), v.end(), greater<string>());
-    
-    string out;
-    for(auto a:v) {
-        out += a;
+
+    sort(v.rbegin(), v.rend());
+    for(auto& s:v) {
+        ret += s;
     }
-    
-    return out;
+    return ret;
 }
 
 int main() {
     printf("%s\n", find("11011000").c_str()); //"11100100"
     printf("%s\n", find("10").c_str()); //"10"
-    
     return 0;
 }
