b49fc9e194d04691074017e3fbe36bd234d0d570
hugochang1 <sand208102@gmail.com>
2026-06-24 14:14:13 +0800

    Update 0540_SingleElementInASortedArray.cpp

--- Programming/Leetcode/NV/0540_SingleElementInASortedArray.cpp
+++ Programming/Leetcode/NV/0540_SingleElementInASortedArray.cpp
@@ -1,32 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <vector>
 
 using namespace std;
-
 //540_SingleElementInASortedArray
-//time complexity: O(logn)
-int recu(vector<int>& nums, int left, int right) {
-    if(left == right) {
-        return left;
-    }
-    
-    int m1 = (right+left)/2;
-    int m2 = m1 ^ 1;
-    
-    if(nums[m1] == nums[m2]) {
-        int mid = max(m1, m2) + 1;
-        return recu(nums, mid, right);
-    } else {
-        int mid = min(m1, m2);
-        return recu(nums, left, mid);
-    }
-}
 
 int find(vector<int> nums) {
-    int left = 0;
-    int right = nums.size() - 1;
-    return recu(nums, left, right);
+  int left = 0;
+  int right = nums.size() - 1;
+  while(left < right) {
+    int mid1 = (left + right) / 2;
+    int mid2 = mid1 ^ 1;
+    if(nums[mid1] == nums[mid2]) {
+      //search right part
+      left = max(mid1, mid2) + 1;
+    } else {
+      //search left part
+      right = min(mid1, mid2);
+    }
+  }
+  return left;
 }
 
 int main() {
@@ -35,4 +27,5 @@ int main() {
     printf("%d\n", find({1,1,2,3,3})); //2
     printf("%d\n", find({1,1,2})); // 2
     printf("%d\n", find({1,2,2})); // 0
+    return 0;
 }
