70f9a64c07b8a2150bbeccd961345448b1896178
hugochang1 <sand208102@gmail.com>
2026-06-24 13:56:50 +0800

    Update 0526_BeautifulArrangement.cpp

--- Programming/Leetcode/NV/0526_BeautifulArrangement.cpp
+++ Programming/Leetcode/NV/0526_BeautifulArrangement.cpp
@@ -4,44 +4,58 @@
 using namespace std;
 //526_BeautifulArrangement
 
-void recu(vector<int>& nums, int n, int index, vector<vector<int>>& ret) {
-    if(n == index) {
-        for(int i = 0; i < n; i++) {
-            if(nums[i] % (i+1) == 0 || (i+1) % nums[i] == 0) continue;
-            else return;
+void recur(vector<int>& v, int index, vector<vector<int>>& out) {
+    if(index == v.size()) {
+        bool check = true;
+        for(int i = 0; i < v.size(); i++) {
+            if(v[i] % (i+1) != 0 && (i+1) % v[i] != 0) {
+                check = false;
+                break;
+            }
         }
-        ret.push_back(nums);
-        return;
+        if(check) {
+            out.push_back(v);
+        }
     }
-
-    for(int i = index; i < n; i++) {
-        swap(nums[index], nums[i]);
-        recu(nums, n, index + 1, ret);
-        swap(nums[index], nums[i]);
+    for(int i = index; i < v.size(); i++) {
+        swap(v[i], v[index]);
+        recur(v, index + 1, out);
+        swap(v[i], v[index]);
     }
 }
 
 vector<vector<int>> solution(int n) {
     vector<vector<int>> ret;
-    vector<int> nums;
-    for(int i = 0; i < n; i++) {
-        nums.push_back(i+1);
+    vector<int> v;
+    for(int i = 1; i <= n; i++) {
+        v.push_back(i);
     }
-    recu(nums, n, 0, ret);
+    recur(v, 0, ret);
     return ret;
 }
 
 int main() {
     vector<vector<int>> ret;
     ret = solution(4);
+    printf("size=%d\n", ret.size());
     for(auto& r:ret) {
         printf("{");
         for(auto& a:r) {
             printf("%d ", a);
         }
-        printf("} ");
+        printf("}\n");
     }
     printf("\n");
-
+    /*
+    size=8
+    {1 2 3 4 }
+    {1 4 3 2 }
+    {2 1 3 4 }
+    {2 4 3 1 }
+    {3 2 1 4 }
+    {3 4 1 2 }
+    {4 2 3 1 }
+    {4 1 3 2 }
+    */
     return 0;
 }
